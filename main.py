17f3b81060b692db0fd0152b445b346242c8041b
defprintcode <74274137+defprintcode@users.noreply.github.com>
2021-01-04 19:32:28 +0100

    Add file name duplication
    add 2 functions for detect and rename files with same name

--- main.py
+++ main.py
@@ -3,44 +3,86 @@ import time
 import datetime
 import shutil
 import json
+import re
 
 
-def readdownload():
+def read_download():
     with open('data.json', 'r') as f:
         extensions_dic = json.load(f)
 
     home = os.path.expanduser("~")
     folder = os.path.join(home, "Downloads")
-    old = folder + '\\' + extensions_dic[0]['folder_name']
+    original_path = folder + '\\' + extensions_dic[0]['folder_name']
+
+    def add_version(path, version):
+        xx = file.name
+        old_ext = re.findall(r'\(\d\)\.\w+\S+$', xx)
+
+        if not len(old_ext):
+            x = xx.find(".")
+            version = "(" + str(version) + ")"
+            output_line = file.name[:x] + version + file.name[x:]
+            scan_coincidences(path, output_line)
+
+        else:
+
+            old_version = re.findall(r'\d', old_ext[0])
+            new_version = int(old_version[0]) + 1
+            new_ext = str(old_ext[0]).replace(str(old_version[0]), str(new_version))
+            new_file = xx.replace(old_ext[0], new_ext)
+            scan_coincidences(path, new_file)
+
+    def scan_coincidences(path, name):
+        i = 1
+        with os.scandir(path) as path_files:
+            for path_file in path_files:
+                if path_file.name == name:
+                    i += i
+        i = i - 1
+
+        if i > 0:
+
+            add_version(path, i)
+        else:
+
+            new_version_path = path + '\\' + name
+            print(new_version_path)
+            shutil.move(original_path, new_version_path)
 
     for extension in extensions_dic:
 
-        with os.scandir(folder) as ficheros:
+        with os.scandir(folder) as files:
 
-            for fichero in ficheros:
+            for file in files:
 
                 if extension['name'] != "inactive":
 
-                    if not os.path.isdir(old + '\\' + extension['path']):
-                        os.mkdir(old + '\\' + extension['path'])
+                    if not os.path.isdir(original_path + '\\' + extension['path']):
+                        os.mkdir(original_path + '\\' + extension['path'])
                         print("Making folder for " + extension['name'])
 
-                    if fichero.name.endswith(tuple(extension['extensions'])) or extension['name'] == "other":
-                        # fichero.name
+                    if file.name.endswith(tuple(extension['extensions'])) or extension['name'] == "other":
+                        # file.name
                         # Get modified time
-                        modified = os.path.getmtime(fichero)
+                        modified = os.path.getmtime(file)
                         x = datetime.datetime.strptime(time.ctime(modified), "%a %b %d %H:%M:%S %Y")
                         y = datetime.datetime.strptime(time.ctime(), "%a %b %d %H:%M:%S %Y")
                         lastime = y - x
 
                         if lastime > datetime.timedelta(extensions_dic[0]['days']):
-                            print(fichero.name)
+                            print(file.name)
                             print("Modified")
                             print(lastime)
-                            shutil.move(folder + '\\' + fichero.name,
-                                        old + '\\' + extension['path'] + '\\' + fichero.name)
+                            # shutil.move(folder + '\\' + file.name,
+                            #            original_path + '\\' + extension['path'] + '\\' + file.name)
+                            # seacrh coincidences
+                            print(folder + '\\' + file.name)
+                            print(original_path + '\\' + extension['path'] + '\\' + file.name)
+
+                            scan_coincidences(folder + '\\' + file.name,
+                                              original_path + '\\' + extension['path'] + '\\' + file.name)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    readdownload()
+    read_download()
